1e469b6c92e1f258d63bda0d13110bf52a82efc7
wlricardo <wlricardo@gmail.com>
2021-02-27 09:16:32 -0300

    Calculando o número de classes

--- EstatisticasBasicas/EstatisticasBasicas.c
+++ EstatisticasBasicas/EstatisticasBasicas.c
@@ -3,10 +3,26 @@
 // dados agrupados em classes, e algumas estatisticas básicas, como Média, 
 // Moda, Mediana, Variância e Desvio-padrão.
 
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
 #include <time.h>
+#include <math.h>
+
+// Funções auxiliares
+
+int menor(int *lista, int tamanho) {
+    int menor = lista[0];
+    for (int i = 1; i < tamanho; i++) {
+        if (lista[i] < menor) {
+            menor = lista[i];
+        }
+    }
+    return menor;
+}
+
 
 int main(int argc, char const *argv[])
 {
@@ -15,7 +31,7 @@ int main(int argc, char const *argv[])
 
     int n;          // Valor informado pelo usuário da quantidade de valores da tabela de dados
     int *tabela;    // Tabela de valores inteiros gerada aleatoriamente, em função de 'n'
-    //int i;
+    int k;          // Informa o número de classes após dados serem agrupados
 
     printf("\n Quantos dados tem sua tabela ? \n > ");
     scanf("%d", &n);
@@ -27,17 +43,24 @@ int main(int argc, char const *argv[])
     }
     
     for (int i = 0; i < n; i++) {
-        tabela[i] = 50 + (rand()%100);
+        tabela[i] = 30 + (rand()%100);
     }
 
     // Imprimindo os dados gerados
+    printf("\n Dados obtidos:\n\n");
     for (int i = 0; i < n; i++) {
-        printf(" %d", tabela[i]);
-        if (i%10 == 0) {
+        printf(" %3d", tabela[i]);
+        if ((i+1)%30 == 0) {
             printf("\n");
         }        
     }    
     
+    k = round(sqrt(n));
+
+    printf("\n\n Preparacao para agrupamento de dados:");
+    printf("\n Número de classes: %d", k);
+    printf("\n Menor valor da tabela de dados: %d", menor(tabela, n));
+
     return 0;
 }
 
